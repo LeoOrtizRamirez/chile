7cce98b313f54be5fce6232b2cbfb9ba75eec295
giraldo.mauricio97@gmail.com <giraldo.mauricio97@gmail.com>
2022-11-30 17:20:08 -0500

    Se agrega el Paginador de contratos

--- resources/js/Pages/Contrato/Index.vue
+++ resources/js/Pages/Contrato/Index.vue
@@ -3,6 +3,43 @@
         <template #header>
             <Busqueda_Rapida></Busqueda_Rapida>
         </template>
+
+        <nav aria-label="Page navigation example">
+            <ul class="inline-flex items-center -space-x-px">
+                <li>
+                    <a href="#" v-on:click="getPreviousPage()"
+                        class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <span class="sr-only">Previous</span>
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </li>
+                
+
+                <li>
+                    <a href="#" v-for="paginas in totalPaginas()" v-on:click="getDataPagina(paginas)" v-bind:class="isActive(paginas)"
+                        class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{ paginas }}</a>
+                </li>
+
+                <li>
+                    <a href="#" v-on:click="getNextPage()"
+                        class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <span class="sr-only">Next</span>
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
         <div class="w-full mt-6">
             <div class="bg-white overflow-auto">
                 <table class="table w-full bg-white border border-indigo-600">
@@ -73,7 +110,7 @@
                         </tr>
                     </thead>
                     <tbody class="text-gray-700">
-                        <tr v-for="contrato in contratos" :key="contrato.id">
+                        <tr v-for="contrato in datosPaginados" :key="datosPaginados.id">
                             <td
                                 class="border border-gray-200 text-left py-3 px-4"
                             >
@@ -216,8 +253,57 @@ import Busqueda_Rapida from "@/components/Busqueda_Rapida.vue";
 export default {
     props: {
         contratos: [],
+
     },
-};
+
+    data() {
+        return {
+            elementosPorPagina: 10,
+            datosPaginados: [],
+            paginaActual: 1
+        }
+    },
+
+    mounted(){
+        this.getDataPagina(1)
+    },
+
+    methods: {
+        totalPaginas() {
+            return Math.ceil(this.contratos.length / this.elementosPorPagina)
+        },
+        getDataPagina(noPagina) {
+            this.paginaActual = noPagina
+            this.datosPaginados = []
+            let ini = (noPagina * this.elementosPorPagina) - this.elementosPorPagina
+            let fin = (noPagina * this.elementosPorPagina)
+
+            /* for (let index = ini; index < fin; index++) {
+                this.datosPaginados.push(this.contratos[index])
+            }
+            console.log(this.datosPaginados) */
+
+            this.datosPaginados = this.contratos.slice(ini,fin)
+        },
+        getPreviousPage(){
+            if(this.paginaActual > 1){
+                this.paginaActual--
+            }
+            this.getDataPagina(this.paginaActual)
+        },
+
+        getNextPage(){
+            if(this.paginaActual < this.totalPaginas()){
+                this.paginaActual++
+            }
+            this.getDataPagina(this.paginaActual)
+        },
+        isActive(noPagina){
+            return noPagina == this.paginaActual ? 'dark:bg-gray-700 dark:text-white' :''
+        }
+    }
+}
+
 </script>
 
 <style lang="scss" scoped>
