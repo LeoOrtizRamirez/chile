667a8d3825c2d0fc5d046221f3f99326dace8656
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2022-12-09 12:01:12 -0500

    Ajustes en vista usuarios, se incluye componente opciones,se agrega acceso directo a usuarios,se ponen estilos e iconos correspondientes

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -78,6 +78,16 @@ const logout = () => {
                                     Contratos
                                 </NavLink>
                             </div>
+                            <div
+                                class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"
+                            >
+                                <NavLink
+                                    :href="route('usuarios.index')"
+                                    :active="route().current('usuarios.index')"
+                                >
+                                    Usuarios
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
